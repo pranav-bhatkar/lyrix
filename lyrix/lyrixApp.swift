bf9438f1b95cac07f13a79ced34685225587178a
Pranav <pranavbhatkar789@gmail.com>
2025-12-30 10:48:11 +0530

    Initial Commit

---
+++ lyrix/lyrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  lyrixApp.swift
+//  lyrix
+//
+//  Created by Pranav Bhatkar on 30/12/25.
+//
+
+import SwiftUI
+
+@main
+struct lyrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
